43414bc4af0bc38bc06a2e6054880aab64e3e371
suolenkainen <pmarjama@gmail.com>
2022-01-02 00:50:17 +0200

    Hand can be created

--- src/visualize.py
+++ src/visualize.py
@@ -1,18 +1,42 @@
 """This visualizes the card game in terminal and later to a text file"""
 
-# variables
-faces = ["1","2","3","4","5","6","7","8","9","10","J","Q","K"]
-suits = ["hearts", "spades", "clubs", "diamonds"]
-# unicode
-"""In Unicode	U+2660 ♠ BLACK SPADE SUIT
-U+2661 ♡ WHITE HEART SUIT
-U+2662 ♢ WHITE DIAMOND SUIT
-U+2663 ♣ BLACK CLUB SUIT
-U+2664 ♤ WHITE SPADE SUIT
-U+2665 ♥ BLACK HEART SUIT
-U+2666 ♦ BLACK DIAMOND SUIT
-U+2667 ♧ WHITE CLUB SUIT"""
+import random
 
-for suit in suits:
+# # variables
+# faces = ["1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+# suits = ["hearts", "spades", "diamonds", "clubs"]
+# suits_uni = ["\u2661", "\u2664", "\u2662", "\u2667"]
+
+# variables
+amount_of_players = 2
+amount_of_card_in_hand = 3
+faces = ["1","2","3","4","5"]
+
+suits = ["hearts", "spades", "diamonds"]
+suits_uni = ["\u2661", "\u2664", "\u2662"]
+
+deck = []
+
+for suit in suits_uni:
     for card in faces:
         print(suit, card)
+        deck.append(suit + card)
+
+print(deck)
+
+# Shuffle the deck
+random.shuffle(deck)
+print(deck)
+
+# Create hands for each player
+players = []
+for _ in range(amount_of_players):
+    hand = []
+    for _ in range(amount_of_card_in_hand):
+        hand.append(deck.pop(0))
+    players.append(hand)
+    print(hand)
+print(deck)
+print(players)
+
